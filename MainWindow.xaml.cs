c711b189d7ccaeebc55049faa7165b6e7f687d6c
student-a502 <student-a502@STUD-RM21-A502>
2025-11-24 14:31:36 +0500

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Time_Lord_Peshin.Pages; // Добавьте эту строку
+
+namespace Time_Lord_Peshin
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            OpenPages(pages.stopwatch);
+        }
+
+        public enum pages
+        {
+            stopwatch
+        }
+
+        public void OpenPages(pages _page)
+        {
+            if (_page == pages.stopwatch)
+                freme.Navigate(new Stopwatch()); 
+        }
+    }
+}
---
+++ Pages/Stopwatch.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Threading;
+
+namespace Time_Lord_Peshin.Pages
+{
+    public partial class Stopwatch : Page
+    {
+        private DispatcherTimer dispatcherTimer = new DispatcherTimer();
+        private int fullSeconds = 0;
+        private bool isRunning = false;
+
+        public Stopwatch()
+        {
+            InitializeComponent();
+            InitializeTimer();
+        }
+
+        private void InitializeTimer()
+        {
+            dispatcherTimer.Tick += TimerTick;
+            dispatcherTimer.Interval = TimeSpan.FromSeconds(1);
+            UpdateDisplay();
+        }
+
+        private void TimerTick(object sender, EventArgs e)
+        {
+            fullSeconds++;
+            UpdateDisplay();
+        }
+
+        private void UpdateDisplay()
+        {
+            int hours = fullSeconds / 3600;
+            int minutes = (fullSeconds % 3600) / 60;
+            int seconds = fullSeconds % 60;
+
+            time.Content = $"{hours:00}:{minutes:00}:{seconds:00}";
+        }
+
+        private void StartStopwatch(object sender, RoutedEventArgs e)
+        {
+            if (!isRunning)
+            {
+                // Запускаем таймер
+                dispatcherTimer.Start();
+                isRunning = true;
+                start.Content = "Стоп";
+                start.Background = new SolidColorBrush(Colors.LightCoral);
+            }
+            else
+            {
+                // Останавливаем таймер
+                dispatcherTimer.Stop();
+                isRunning = false;
+                start.Content = "Начать";
+                start.Background = new SolidColorBrush(Colors.White);
+            }
+        }
+        private void ResetStopwatch(object sender, RoutedEventArgs e)
+        {
+            dispatcherTimer.Stop();
+            fullSeconds = 0;
+            isRunning = false;
+            start.Content = "Начать";
+            start.Background = new SolidColorBrush(Colors.White);
+            UpdateDisplay();
+        }
+    }
+}
